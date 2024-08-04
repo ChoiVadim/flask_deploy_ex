b3d27f9226efc9e03914a8c22b3bb312830201f8
vadim_lixun_server <tsoivadim97@gmail.com>
2024-08-05 00:31:12 +0900

    add login and index page

--- app/test/routes.py
+++ app/test/routes.py
@@ -4,18 +4,8 @@ test_routes = Blueprint('test', __name__, template_folder="templates")
 
 @test_routes.route('/')
 def home():
-    msg = "Hello, vadim"
-    return render_template("main/index.html", message=msg)
+    return render_template("index.html")
 
 @test_routes.route('/login')
 def login():
-    username = request.form.get("username")
     return render_template("login.html")
-
-@test_routes.route('/test')
-def test():
-    return redirect(url_for('new'))
-
-@test_routes.route('/new')
-def new():
-    return render_template('new.html', name='vadim')
