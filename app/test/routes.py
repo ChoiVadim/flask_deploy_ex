32b64127e9b598c2c32c3e79caeac4048fc9663f
vadim_lixun_server <tsoivadim97@gmail.com>
2024-08-06 02:38:12 +0900

    add upload and download routes

--- app/test/routes.py
+++ app/test/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, request, url_for, redirect, Blueprint, session
+import os
+from flask import render_template, request, url_for, redirect, Blueprint, session, send_from_directory
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
 from app.test.models import User
 from app.app import db
+from app.config import Config
 
 
 test_routes = Blueprint("test", __name__, template_folder="templates")
@@ -64,6 +67,37 @@ def switch():
     return render_template("switch.html")
 
 
-@test_routes.route("/upload", methods=["GET"])
+def allowed_file(filename):
+    allowed_extensions = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+@test_routes.route("/upload", methods=["GET", "POST"])
 def upload():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return render_template("upload.html", message='No file part')
+        
+        file = request.files['file']
+        
+        if file.filename == '':
+            return render_template("upload.html", message='No selected file')
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(Config.UPLOAD_FOLDER, filename))
+            return render_template("upload.html", message='File successfully uploaded')
+        else:
+            return render_template("upload.html", message='Invalid file type')
+    
     return render_template("upload.html")
+
+@test_routes.route("/files")
+def list_files():
+    files = os.listdir(Config.UPLOAD_FOLDER)
+    return render_template("files.html", files=files)
+
+@test_routes.route('/download/<path:filename>', methods=['GET', 'POST'])
+def download_file(filename):
+    uploads = os.path.join("/home/vadim/Desktop/flask_app/", Config.UPLOAD_FOLDER)
+    return send_from_directory(uploads, filename)
